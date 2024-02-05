58cc60bb6b54946289b73967009e70a168c9d133
MSpinczyk <michal.spinczyk@gmail.com>
2024-02-05 21:15:49 +0100

    Changes in bpmn_transformation.py

--- src/bpmn_transformation.py
+++ src/bpmn_transformation.py
@@ -1,28 +1,12 @@
-from bpmn_python.bpmn_diagram_rep import BpmnDiagramGraph
-from bpmn_python.bpmn_diagram_export import  BpmnDiagramGraphExport
-from bpmn_python.bpmn_process_csv_import import BpmnDiagramGraphCSVImport
+import bpmn_python.bpmn_diagram_rep as diagram
+import bpmn_python.bpmn_diagram_layouter as layouter
+
+import os
 
 def transform_to_bpmn(spreadsheet_path, bpmn_output_path):
-    bpmn_importer = BpmnDiagramGraphCSVImport()
+    bpmn_graph = diagram.BpmnDiagramGraph()
+    bpmn_graph.load_diagram_from_csv_file(os.path.abspath(spreadsheet_path))
+    layouter.generate_layout(bpmn_graph)
+    bpmn_graph.export_xml_file('output/', bpmn_output_path)
 
-    # Assume bpmn_diagram is an instance of your BPMN diagram class
-    # e.g., bpmn_diagram = YourBpmnDiagramClass()
-
-    # Provide the file path to your CSV file
-    csv_filepath = 'test.csv'
-    bpmn_diagram = BpmnDiagramGraph()
-    # Call the load_diagram_from_csv method to import data from the CSV file
-    bpmn_importer.load_diagram_from_csv(csv_filepath, bpmn_diagram)
-
-    
-    # Add nodes and edges to the bpmn_diagram...
-
-    # Specify the output directory and filename for the BPMN XML file
-    output_directory = "./output/"
-    output_filename = "exported_bpmn.xml"
-
-    # Export BPMN diagram to BPMN XML file with Diagram Interchange data
-    BpmnDiagramGraphExport.export_xml_file(output_directory, output_filename, bpmn_diagram)
-    pass
-
-transform_to_bpmn('x','y')
+transform_to_bpmn('test.csv', 'test_2.bpmn')
